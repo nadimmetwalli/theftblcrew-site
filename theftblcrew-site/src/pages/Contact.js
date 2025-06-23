42d5df0b98e20d9c3465d194109b8baefddb842b
Nadim Metwalli <nadimmetwalli@gmail.com>
2025-06-23 18:52:02 +0300

    Structure done

--- theftblcrew-site/src/pages/Contact.js
+++ theftblcrew-site/src/pages/Contact.js
@@ -1,8 +1,23 @@
+import './Contact.css';
+
 function Contact() {
     return(
-        <div>
-            <p>Contact Us</p>
+        <div className="contact-page">
+            <h1>Contact us</h1>
+            <p>Want to work with theftblcrew or have a question?</p>
+            <p>📧 Email us at: nadimmetwalli@gmail.com</p>
+            <p>📩 Or DM us on Instagram: <a href="https://instagram.com/theftblcrew" target="_blank">@theftblcrew</a></p>
+
+            <form>
+                <input type="text" placeholder="Your name"/>
+                <input type="email" placeholder="Your email"/>
+                <textarea placeholder="Your message"/>
+                <button type="submit">Send</button>
+            </form>
         </div>
+
+
+        
     )
 }
 export default Contact;
--- theftblcrew-site/src/pages/Services.js
+++ theftblcrew-site/src/pages/Services.js
@@ -1,8 +1,38 @@
+import { Link } from 'react-router-dom';
+import './Services.css';
+
+
 function Services() {
     return(
         <div>
             <h1>Our Services</h1>
-            <p>This is the Services page.</p>
+            <h2>What We Offer</h2>
+
+            <ul>
+                <li>Shoutouts</li>
+                <li>Post Promotions</li>
+                <li>Brand collaborations</li>
+                <li>Music Promotions</li>
+            </ul>
+
+            <h2>Content Formats</h2>
+            <p>We offer Reels, Stories, Carousels, and more.</p>
+
+            <h2>Why Work With Us?</h2>
+            <p>Large reach, high engagement, 
+            and guaranteed sales through a loyal football audience.</p>
+
+            <h2>Our Packages</h2>
+            <p>Click a package below to see pricing and what's included.</p>
+
+            <h2>Let's Work Together</h2>
+            <p>
+                Interested? <Link to="/contact">Head over to our contact page</Link>
+                to get in touch!
+            </p>
+
+
+
         </div>
     );
 }
