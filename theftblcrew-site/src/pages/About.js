d2b2cda804efd57cf7db39269e713ea914e5f09f
Nadim Metwalli <nadimmetwalli@gmail.com>
2025-07-10 23:16:47 +0300

    Icon placement

--- theftblcrew-site/src/pages/About.js
+++ theftblcrew-site/src/pages/About.js
@@ -1,5 +1,7 @@
 import './About.css';
 import yamalPhoto from '../assets/yamal.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faVideo } from '@fortawesome/free-solid-svg-icons';
 
 function About() {
   return (
@@ -20,29 +22,31 @@ function About() {
         </div>
       </div>
 
-      <h2>What is theftblcrew</h2>
-      <p>@theftblcrew is a football-focused <br />
-       Instagram page that delivers <br/>
-      entertaining, engaging content for fans <br/>
-      around the world.</p>
+      <div className='what-we-do-box'>
+        <h2>What do we do?</h2>
+      </div>
 
-      <h2>What kind of content do we post?</h2>
-      <p>We post a mix of entertaining football content—including reels, 
-      edits, iconic moments, memes, and fun takes on trending football topics. 
-      It's not just memes—it's everything that keeps fans entertained.</p>
+      {/* Flex container for the two cards */}
+      <div className="cards-container">
+        <div className='about-card-tilted'>
+          <h2>Content</h2>
+          <FontAwesomeIcon icon={faVideo} size='3x' className='video-icon' />
+          <h3>We post a mix of entertaining football
+           content—including reels, edits, iconic
+          moments, memes, and fun takes on
+          trending football topics. It's not just
+          memes—it's everything that keeps<br/>
+          fans entertained.</h3>
+        </div>
 
-      <h2>Who is your audience?</h2>
-      <p>Our audience is made up of football fans of all ages, 
-      especially Gen Z and Millennials who love fast, fun, and creative content.</p>
-
-      <h2>Our reach</h2>
-      <p>We currently have over 77,000 followers on Instagram and growing steadily.</p>
-      
-      <h2>Our story</h2>
-      <p>We’re a passionate football-loving crew aiming to entertain. 
-      Whether it’s a legendary goal, a funny moment, or an epic comeback, we bring the energy, 
-      nostalgia, and fun side of the game to our followers.</p>
-      
+        <div className='about-card'>
+          <h2>Audience</h2>
+          <h3>Our audience is made up of football <br/>
+          fans of all ages, especially Gen Z and<br/>
+          Millennials who love fast, fun, and <br/>
+          creative content.</h3>
+        </div>
+      </div>
     </div>
   );
 }
